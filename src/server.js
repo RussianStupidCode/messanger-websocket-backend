1a5e4ac226b1894ac7e030fd804c0cf9fa862393
artur <i@aderbin.ru>
2022-07-22 07:10:33 +0500

    change port

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import closeHandler from './handlers.js/close.js';
 import messageHandler from './handlers.js/message.js';
 import ServerModel from './server-model.js';
 
-const PORT = process.env.port || 7070;
+const PORT = process.env.port || 3000;
 const server = http.createServer();
 server.on('request', (request, response) => {
   response.write('hello');
